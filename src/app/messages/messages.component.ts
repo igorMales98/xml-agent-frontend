1d35eb179146b2a241e69be3f75b7aed00ab8792
igorMales98 <igor_malesevic@yahoo.com>
2020-05-09 00:33:33 +0200

    Format messages files.

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {faUserCircle} from '@fortawesome/free-regular-svg-icons';
-import { MessagesService } from './messages.service';
-import { User } from '../model/user';
+import {MessagesService} from './messages.service';
+import {User} from '../model/user';
 import {Message} from '../model/message';
-//TODO: scroll i refresh
+
+// TODO: scroll i refresh
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
@@ -17,38 +18,43 @@ export class MessagesComponent implements OnInit {
   agentId = '1';
   show: boolean;
 
-  constructor(private messagesService: MessagesService) { }
+  constructor(private messagesService: MessagesService) {
+  }
 
   ngOnInit(): void {
     this.messagesService.getReservedCustomers(this.agentId).subscribe(data => {
-      this.customers = data
+      this.customers = data;
     });
   }
 
   showMessages(customer: User) {
-    this.messagesService.getMessages(this.agentId,customer.id).subscribe(data => {
+    this.messagesService.getMessages(this.agentId, customer.id).subscribe(data => {
       this.messages = data;
-      for (let i=0;i<this.messages.length;i++) {
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < this.messages.length; i++) {
         this.messages[i].messageDate = this.formatDate(this.messages[i].messageDate);
-        if (this.messages[i].sender.id == this.agentId)
-          this.messages[i].type = "agent";
-        else 
-          this.messages[i].type = "customer";
+        // tslint:disable-next-line:triple-equals
+        if (this.messages[i].sender.id == this.agentId) {
+          this.messages[i].type = 'agent';
+        } else {
+          this.messages[i].type = 'customer';
+        }
       }
       this.clickedCustomer = customer;
-      (<HTMLInputElement>document.getElementById("newMessage")).value = '';
+      (document.getElementById('newMessage') as HTMLInputElement).value = '';
     });
   }
 
   sendMessage() {
-    let body = (<HTMLInputElement>document.getElementById("newMessage")).value;
-    let message = new Message(body,this.clickedCustomer);
+    const body = (document.getElementById('newMessage') as HTMLInputElement).value;
+    const message = new Message(body, this.clickedCustomer);
     this.messagesService.sendMessage(message).subscribe();
   }
- 
+
   formatDate(oldDate: string) {
     let newDate;
-    newDate = (+oldDate[3]<10?("0"+oldDate[3]):oldDate[3])+":"+(+oldDate[4]<10?("0"+oldDate[4]):oldDate[4])+' '+oldDate[2]+'.'+oldDate[1]+'.'+oldDate[0];
+    newDate = (+oldDate[3] < 10 ? ('0' + oldDate[3]) : oldDate[3]) + ':' + (+oldDate[4] < 10 ? ('0' + oldDate[4]) : oldDate[4]) + ' '
+      + oldDate[2] + '.' + oldDate[1] + '.' + oldDate[0];
     return newDate;
   }
 }
